c5beca1ed3510ef16665272709019acdeeb4ccb4
Bhumika Agarwal <bhumika.aga@gmail.com>
2023-01-31 23:11:31 +0530

    Chapter 02

---
+++ App.js
@@ -0,0 +1,18 @@
+const heading1 = React.createElement(
+        "h1",
+        { id: "title" },
+        "Hello World!"
+      );
+const heading2 = React.createElement(
+        "h2",
+        { id: "title" },
+        "Hello Bhumika!"
+      );
+const container = React.createElement("div", { id: "container" }, [
+        heading1,
+        heading2,
+      ]);
+// console.log(heading);
+//Creating a root element to inform react
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
