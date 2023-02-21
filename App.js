3e869f7fc08781528cc0da3c51e9a995a6bf9e86
Bhumika Agarwal <bhumika.aga@gmail.com>
2023-02-21 17:33:21 +0530

    Chapter-3

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-        "h1",
-        { id: "title" },
-        "Hello World!"
-      );
-const heading2 = React.createElement(
-        "h2",
-        { id: "title" },
-        "Hello Bhumika!"
-      );
-const container = React.createElement("div", { id: "container" }, [
-        heading1,
-        heading2,
-      ]);
-// console.log(heading);
+//Gives the output of the json object of the element heading1 using JSX
+//React Element
+const heading = (
+  <h1 id="title" key="header">
+    Namaste React
+  </h1>
+);
+
+//React Functional Component
+//Example 1 (Most Used)
+const HeaderComponent1 = () => (
+  <div>
+    <h1 id="title" key="header">
+      Namaste React!
+    </h1>
+    <h2>This is the new heading!</h2>
+  </div>
+);
+
+//Example 2
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/*Using React element in the Functional Component. Won't work if it is made a Functional Component. */}
+      {heading}
+      {/* Component Composition- Using a React Functional Component inside another Functional Component */}
+      <HeaderComponent1 />
+      {/* Using as a function */}
+      {HeaderComponent2()}
+      <h1 id="title" key="header">
+        Namaste React!
+      </h1>
+      <h2>This is the second heading!</h2>
+    </div>
+  );
+};
+
+//Example 3
+const HeaderComponent2 = function () {
+  return (
+    <div>
+      <h1 id="title" key="header">
+        Namaste React!
+      </h1>
+      <h2>This is the novel heading!</h2>
+    </div>
+  );
+};
+
+//Example 4
+const heading1 = () => (
+  //JSX
+  <h1 id="title" key="header">
+    Namaste React
+  </h1>
+);
+
 //Creating a root element to inform react
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+// root.render(heading);
+root.render(<HeaderComponent />); //To render a functional component to root
